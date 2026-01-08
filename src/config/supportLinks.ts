f1042b927aa9e2e5457c50ba354a12b26384179f
Alexander Lunin <info@aibyteconsult.com>
2026-01-08 20:58:41 +0000

    Apply latest updates 5

--- src/config/supportLinks.ts
+++ src/config/supportLinks.ts
@@ -3,18 +3,18 @@ export const SUPPORT_LINKS = {
     stripe: 'https://donate.stripe.com/bJe8wOdvU0f29mJ5l3aVa13',
     revolut: 'https://checkout.revolut.com/pay/ec702497-bab3-4fd3-81be-6ff8926b3bb3',
     paypal: 'https://www.paypal.com/ncp/payment/9F48QUYZ5D42G',
-    coinbase: 'https://commerce.coinbase.com/checkout/ec1119d3-4f56-4d51-89cc-956af0b851db',
+    coinbase: 'https://commerce.coinbase.com/checkout/468f3710-4fb6-48cd-b6b5-f547fc89cf29',
   },
   founding: {
     stripe: 'https://donate.stripe.com/14A6oG4Zof9W0QdfZHaVa14',
     revolut: 'https://checkout.revolut.com/pay/e0dd516f-47da-4436-9262-0ba33d428012',
-    paypal: 'hhttps://www.paypal.com/ncp/payment/KFYULFX7SMC8Y',
-    coinbase: 'https://commerce.coinbase.com/checkout/147565e8-9550-490d-abc7-99f47097b1f2',
+    paypal: 'https://www.paypal.com/ncp/payment/KFYULFX7SMC8Y',
+    coinbase: 'https://commerce.coinbase.com/checkout/03152a38-7bc1-410d-a3f6-7a4586054557',
   },
   vip: {
     stripe: 'https://donate.stripe.com/aFa4gycrQ5zmbuR00JaVa15',
     revolut: 'https://checkout.revolut.com/pay/c78dcd5f-7110-457f-af41-422b6885b3bc',
     paypal: 'https://www.paypal.com/ncp/payment/VTV4HD37FUZK2',
-    coinbase: 'https://commerce.coinbase.com/checkout/83c1716b-c032-4e52-b7b5-efbc01cd57b3',
+    coinbase: 'https://commerce.coinbase.com/checkout/ad6d5849-9791-4375-a297-c154f4dcbd8e',
   },
 } as const;
